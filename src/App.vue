7808ceaac6b510aa677fcd9ee9320ff44c7b83e2
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-04-24 23:02:40 +0800

    Chore: css reset

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,67 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    hello world
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
+@Component
 export default class App extends Vue {}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  * {
+    -webkit-box-sizing: border-box;
+    -moz-box-sizing: border-box;
+    box-sizing: border-box;
+  }
+
+  html, body, div, span, applet, object, iframe,
+  h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+  a, abbr, acronym, address, big, cite, code,
+  del, dfn, em, font, img, ins, kbd, q, s, samp,
+  small, strike, strong, sub, sup, tt, var,
+  dl, dt, dd, ol, ul, li,
+  fieldset, form, label, legend,
+  table, caption, tbody, tfoot, thead, tr, th, td {
+    margin: 0;
+    padding: 0;
+    border: 0;
+    outline: 0;
+    font-weight: inherit;
+    font-style: inherit;
+    font-size: 100%;
+    font-family: inherit;
+    vertical-align: baseline;
+  }
+  /* remember to define focus styles! */
+  :focus {
+    outline: 0;
+  }
+  body {
+    line-height: 1;
+    color: black;
+    background: white;
+  }
+  ol, ul {
+    list-style: none;
+  }
+  /* tables still need 'cellspacing="0"' in the markup */
+  table {
+    border-collapse: separate;
+    border-spacing: 0;
+  }
+  caption, th, td {
+    text-align: left;
+    font-weight: normal;
+  }
+  blockquote:before, blockquote:after,
+  q:before, q:after {
+    content: "";
+  }
+  blockquote, q {
+    quotes: "" "";
+  }
 </style>
