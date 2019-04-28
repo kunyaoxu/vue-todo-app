825488a2acaf1cd56fe7995e30167954b0422ca6
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-04-29 02:47:24 +0800

    Chore: vuex store
    Add navState state.
    Add setNavState function.

--- src/store.ts
+++ src/store.ts
@@ -5,10 +5,12 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    navState: '0',
   },
   mutations: {
-
+    setNavState(state, newNavState) {
+      state.navState = newNavState;
+    },
   },
   actions: {
 
