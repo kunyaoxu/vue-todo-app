e9026d12d1020cf6bddf7563f3cf01cc7e5ca439
kunyaoxu <29974104+kunyaoxu@users.noreply.github.com>
2019-04-29 22:14:41 +0800

    Feat: add todoList to store
    Declare ITodo interface.
    Make fake data for todoList.

--- src/store.ts
+++ src/store.ts
@@ -3,13 +3,35 @@ import Vuex from 'vuex';
 
 Vue.use(Vuex);
 
+export interface ITodo {
+  id: string;
+  text: string;
+  isFinished: boolean;
+}
+
+const todoList: ITodo[] = [
+  {
+    id: 'qweqwe789789',
+    text: 'i am inital todo',
+    isFinished: false,
+  },
+];
+
 export default new Vuex.Store({
   state: {
     navState: '0',
+    todoList,
   },
   mutations: {
-    setNavState(state, newNavState) {
+    setNavState(state, newNavState: string) {
       state.navState = newNavState;
+    },
+    onChangeFinished(state, id: string) {
+      state.todoList.forEach((el) => {
+        if (el.id === id) {
+          el.isFinished = !el.isFinished;
+        }
+      });
     },
   },
   actions: {
